6048ad2aa2804ed8681f1729f93619e52b4e325b
abdullo Nishanov <abdullohnishanov2007@gmail.com>
2025-03-19 15:07:02 +0600

    commint 4

--- src/pages/VideoCourses/VideoCourses.jsx
+++ src/pages/VideoCourses/VideoCourses.jsx
@@ -27,6 +27,31 @@ const VideoCourses = () => {
             description: 'Изучите основы веб-разработки с помощью HTML и CSS.',
             link: 'https://example.com/html-css-course',
             image: 'https://th.bing.com/th/id/OIP.jkucKKfidsEUxAOwrUyM1AHaFF?w=1024&h=703&rs=1&pid=ImgDetMain' // Замените на URL вашего изображения
+        },
+
+        {
+            title: 'Курс по JavaScript',
+            description: 'Изучите основы JavaScript и создайте свои первые веб-приложения.',
+            link: 'https://example.com/javascript-course',
+            image: 'https://itproger.com/img/courses/1476977754.jpg' // Замените на URL вашего изображения
+        },
+        {
+            title: 'Курс по React',
+            description: 'Углубитесь в React и научитесь создавать интерактивные пользовательские интерфейсы.',
+            link: 'https://example.com/react-course',
+            image: 'https://itproger.com/img/courses/x1494770683.jpg.pagespeed.ic.ovSnDXpXaC.webp' // Замените на URL вашего изображения
+        },
+        {
+            title: 'Курс по Python',
+            description: 'Научитесь программировать на Python и создавайте свои собственные проекты.',
+            link: 'https://example.com/python-course',
+            image: 'https://itproger.com/img/intensivs/python.svg' // Замените на URL вашего изображения
+        },
+        {
+            title: 'Курс по HTML и CSS',
+            description: 'Изучите основы веб-разработки с помощью HTML и CSS.',
+            link: 'https://example.com/html-css-course',
+            image: 'https://th.bing.com/th/id/OIP.jkucKKfidsEUxAOwrUyM1AHaFF?w=1024&h=703&rs=1&pid=ImgDetMain' // Замените на URL вашего изображения
         }
     ];
 
